c2ec2ab77b715ef7d0b522abe426ebc2a3e80509
Ehsan Shaghaei <53513242+Ehsan2754@users.noreply.github.com>
2021-09-17 18:39:59 +0300

    packageing

---
+++ HOLOEYE-PLUTO-2-EMBEDDED/__meta__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+__all__ = [
+    "__packagename__",
+    "__version__",
+    "__summary__",
+    "__url__",
+    "__author__",
+    "__email__",
+    "__license__"
+]
+
+# The name of the package 
+__packagename__ = "HOLOEYE-PLUTO-2-EMBEDDED"
+
+# A version number like 3.1.4 or 1.0a3 
+__version__ = "0.1.0"
+
+# A one-line summary of what the package does. It's like the first line of a doc string. 
+__summary__ = "This repo is an alternative interface to official API of the Holoeye PLUTO-2 Spatial Light Modulator in order to control multiple devices through RNDIS network rather than classic HDMI, though you don't need to buy multiple vide cards for communicating to all your beloved SLMs. "
+
+# The URL of the package's home page 
+__url__ = "https://github.com/ehsan2754/HOLOEYE-PLUTO-2-EMBEDDED"
+
+# The name of the author 
+__author__ = "Ehsan Shaghaei"
+
+# The email address of the author. (PEP 241 mentions that this might be used as a unique key in some catalog of packages, but I don't know if it actually is.) 
+__email__ = "e.shaghaei@innopolis.university"
+
+# PEP 241 says to put the name of the license here, but I don't think that's a good idea. The name doesn't identify a specific license. There are several Zope licenses and several PSF licenses. I recommend putting the URL of the license. 
+__license__ = "MIT"
---
+++ HOLOEYE-PLUTO-2-EMBEDDED/docs/source/index.rst
@@ -0,0 +1,22 @@
+.. HOLOEYE-PLUTO-2-EMBEDDED documentation master file, created by
+   sphinx-quickstart on Sat May 17 11:44:24 2014.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to HOLOEYE-PLUTO-2-EMBEDDED's documentation!
+======================================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
